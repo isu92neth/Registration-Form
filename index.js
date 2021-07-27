a0c15ed97c22bd2055af004d5dea217720360bb0
isu92neth <123csissa@gmail.com>
2021-07-28 02:46:57 +0530

    Added input format validations

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//SparkX Assignment
+//Registration form for a library
+//form validations
+
+//get the elements for the specified IDs related to all input fields
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const email = document.getElementById("email");
@@ -5,62 +10,74 @@ const address = document.getElementById("address");
 const contact = document.getElementById("contact");
 const payment = document.getElementById("payment");
 const userTypes = document.getElementsByName("userTypes");
-const school = document.getElementById("school");
-const university = document.getElementById("university");
-const employee = document.getElementById("employee");
 const form = document.getElementById("registration");
 
+//used for input format validations
 const alphaLetters = /^[A-Za-z]+$/;
 const numbers = /^[0-9]+$/;
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+//input validation function
+//check if there are empty input fields
+//check for correct input formats
+//on invalid input form submission fails
 form.addEventListener("submit", (e) => {
+  //variable to check if all input fields are validated
+  let success = true;
+
+  //validating First Name input
   if (fname.value === "" || fname.value === null) {
     e.preventDefault();
     fnameError.innerText = "Required";
-    console.log("requiirreeddd");
+    success = false;
   } else if (!fname.value.match(alphaLetters)) {
     e.preventDefault();
     fnameError.innerText = "Only allowed alphabetical letters";
-    console.log("requiirreeddd");
+    success = false;
   }
 
+  //validating Last Name input
   if (lname.value === "" || lname.value === null) {
     e.preventDefault();
     lnameError.innerText = "Required";
-    console.log("requiirreeddd");
+    success = false;
   } else if (!lname.value.match(alphaLetters)) {
     e.preventDefault();
     lnameError.innerText = "Only allowed alphabetical characters";
-    console.log("requiirreeddd");
+    success = false;
   }
 
+  //validating Email Address input
   if (email.value === "" || email.value === null) {
     e.preventDefault();
     emailError.innerText = "Required";
-    console.log("requiirreeddd");
+    success = false;
   } else if (!email.value.match(emailFormat)) {
     e.preventDefault();
     emailError.innerText = "Invalid email address";
-    console.log("requiirreeddd");
+    success = false;
   }
 
+  //validating Contact Number input
   if (contact.value === "" || contact.value === null) {
     e.preventDefault();
     contactError.innerText = "Required";
-    console.log("requiirreeddd");
+    success = false;
   } else if (!contact.value.match(numbers)) {
     e.preventDefault();
     contactError.innerText = "Only allowed numerical characters";
-    console.log("requiirreeddd");
+    success = false;
   }
 
+  //validating Address input
   if (address.value === "" || address.value === null) {
     e.preventDefault();
     addressError.innerText = "Required";
-    console.log("requiirreeddd");
+    success = false;
   }
 
+  //validating User Type radio button input
+  //user is only allowed select one option
   var x = 0;
 
   for (let i = 0; i < userTypes.length; i++) {
@@ -71,17 +88,24 @@ form.addEventListener("submit", (e) => {
 
   if (x != 1) {
     e.preventDefault();
-    userTypeError.innerText = "Select one option";
-    console.log("requiirreeddd");
+    userTypeError.innerText = "Required (Select one option)";
+    success = false;
   } else {
     userTypeError.innerText = "";
   }
+
+  //on validation success form submission will notify with a success alert message
+  if (success) {
+    alert("User Added Successfully");
+  }
 });
 
+//function to hide error messages
 function hideError() {
   document.getElementById(this.id + "Error").innerHTML = "";
 }
 
+//hide error messages on release key
 fname.onkeyup = hideError;
 lname.onkeyup = hideError;
 email.onkeyup = hideError;
